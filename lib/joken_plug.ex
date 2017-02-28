ebcd108a91e4176b57ff70ef75da7fe356d5da39
Sunil Gopinath <sunilgopinath@R5006503.local>
2017-02-28 16:51:06 +1100

    initial commit of plug framework

---
+++ lib/joken_plug.ex
@@ -0,0 +1,18 @@
+defmodule JokenPlug do
+  @moduledoc """
+  Documentation for JokenPlug.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> JokenPlug.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/joken_plug_test.exs
@@ -0,0 +1,8 @@
+defmodule JokenPlugTest do
+  use ExUnit.Case
+  doctest JokenPlug
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
